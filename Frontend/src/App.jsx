3034330a8ba9203520a0c82426fc2ccc1942cc63
nof1eld <imadeddine799@gmail.com>
2025-04-23 10:01:28 +0100

    Update routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import DoctorPanel from "./pages/DoctorPanel.jsx";
-import PatientPanel from "./pages/PatientPanel.jsx";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import { Contact } from "./pages/Contact.jsx"
+import Login from "./pages/Login.jsx"
+import Register from "./pages/Register.jsx"
+import DoctorPanel from "./pages/DoctorPanel.jsx"
+import PatientPanel from "./pages/PatientPanel.jsx"
+import "./App.css"
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/doctor" element={<DoctorPanel />} />
-          <Route path="/patient" element={<PatientPanel />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/doctor-panel" element={<DoctorPanel />} />
+        <Route path="/patient-panel" element={<PatientPanel />} />
+      </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
